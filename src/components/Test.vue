5ece88c73f59410b015a64ad68d7e635fe2d35a6
kikesoy <esoto@ideolab.net>
2019-06-24 17:57:33 -0400

    Probando inputs para rescatar la data interviniendo los modelos (v-model) y escuchar las teclas presionadas con la directiva v-on:keyup

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>Uso de métodos</h1>
     <button type="button" v-on:click="saludar(user.firstName)">Click Me</button>
+    <hr>
+    <input type="text" v-model="user.firstName">
+    <hr>
+    <input type="text" v-on:keyup="typing">
+    <hr>
+    <input type="text" v-on:keyup.enter="typingEnter">
   </div>
 </template>
 
@@ -35,6 +41,12 @@ export default {
   methods: {
     saludar(name) {
       alert("Hola " + name);
+    },
+    typing(e) {
+      console.log(e.target.value);
+    },
+    typingEnter(e) {
+      alert("Presionó la tecla Enter");
     }
   }
 };
